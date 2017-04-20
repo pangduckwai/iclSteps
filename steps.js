417fa09f676bc08b3d0eddbfbe71105f3629e233
icdlct <paul.ct.lai@hkicl.com.hk>
2017-04-20 19:43:09 +0800

    Add logo

--- steps.js
+++ steps.js
@@ -1,48 +1,28 @@
+const request = require('request');
+const http = require('http');
+const path = require('path');
+const url = require('url');
+const os = require('os');
+const fs = require('fs');
+
 // TODO!!! Learn where to put config params in node.js!!!
 const protocol = 'http';
 const bcNodes = [
-	{name : "Node 0", addr : "192.168.14.130", port : "7050",  user : "test_user0", scrt : "MS9qrN8hFjlE"}, //"127.0.0.1" 7050 8050 9050 10050
-	{name : "Node 1", addr : "192.168.14.130", port : "8050",  user : "test_user1", scrt : "jGlNl6ImkuDo"},
-	{name : "Node 2", addr : "192.168.14.130", port : "9050",  user : "test_user2", scrt : "zMflqOKezFiA"},
-	{name : "Node 3", addr : "192.168.14.130", port : "10050", user : "test_user3", scrt : "vWdLCE00vJy0"},
+	{name : "Node 0", addr : "127.0.0.1", port : "7050",  user : "test_user0", scrt : "MS9qrN8hFjlE"}, //127.0.0.1 7050 8050 9050 10050
+	{name : "Node 1", addr : "127.0.0.1", port : "8050",  user : "test_user1", scrt : "jGlNl6ImkuDo"}, //192.168.14.130
+	{name : "Node 2", addr : "127.0.0.1", port : "9050",  user : "test_user2", scrt : "zMflqOKezFiA"},
+	{name : "Node 3", addr : "127.0.0.1", port : "10050", user : "test_user3", scrt : "vWdLCE00vJy0"},
 ];
-
-var buildUi = function(msg, user, yr, mn, dt, step, node, state) {
-	var ro0 = (state != 1) ? 'readonly' : '';
-	var ro1 = ((state == 2) || (state == 3)) ? 'readonly' : '';
-	var fcs = (state == 0) ? "'step'" : "'user'";
-	var act = (state == 0) ? '/submit' : '/verify';
-
-	var hfs = '<a href="/submit?node=' + node + '">';
-	var hfv = '<a href="/verify?node=' + node + '">';
-	var mnu = (state == 0) ? 'Submit | ' + hfv + 'Verify</a>' : ((state == 1) ? hfs + 'Submit</a> | Verify' : 'Submit | Verify');
-
-	var sel = '';
-	if ((state != 2) && (state !=3)) {
-		sel = ' | Connect to <select name="node" value="' + node + '" style="font-size:0.7em;" onchange="this.form.submit()">'
-		for (var i = 0; i < bcNodes.length; i ++) {
-			sel += '<option ' + ((i == node) ? 'selected ' : '') + 'value="' + i + '">' + bcNodes[i].name + '</option>';
-		}
-		sel += '</select>';
-	}
-
-	var rtn = 
-'<html><head><title>HKICL Pedometer Contest</title></head>' +
-'<body style="font-family:sans-serif" onload="document.getElementById(' + fcs + ').focus();">' +
-'<h2>HKICL Pedometer Contest</h2>' +
-'<form action="' + act + '" method="get"><div style="font-size:0.9em;height:21px">' + mnu + sel + '</div></form>' +
-'<div style="font-size:1.1em;height:28px">' + ((msg == '') ? 'Welcome' : msg) + '</div>' +
-'<form action="' + act + '" method="post"><input type="hidden" name="node" value="' + node + '"/><table>' +
-'<tr><td>User: </td><td><input type="text" name="user" id="user" value="' + user + '" ' + ro0 + '/></td></tr>' +
-'<tr><td>Date: </td><td>' +
-'<input type="text" size="4" maxlength="4" name="year" value="' + yr + '" ' + ro1 + '/>' +
-'<input type="text" size="2" maxlength="2" name="mnth" value="' + mn + '" ' + ro1 + '/>' +
-'<input type="text" size="2" maxlength="2" name="date" value="' + dt + '" ' + ro1 + '/></td></tr>' +
-'<tr><td>Steps: </td><td>' +
-'<input type="text" size="7" name="step" id="step" value="' + step + '" onfocus="this.select();" ' + ro1 + '/></td></tr>' +
-'</table><br/>' + ((state == 2) ? hfs + 'Return</a></div>' : ((state == 3) ? hfv + 'Return</a></div>' : '<input type="submit" value="Send"/><input type="reset" value="Clear"/>')) +
-'</form></body></html>';
-	return (rtn);
+const mimeMap = {
+	'.ico' : 'image/x-icon',
+    '.html': 'text/html',
+    '.js'  : 'text/javascript',
+    '.json': 'application/json',
+    '.css' : 'text/css',
+    '.png' : 'image/png',
+    '.jpg' : 'image/jpeg',
+    '.svg' : 'image/svg+xml',
+    '.pdf' : 'application/pdf'
 };
 
 var rspnErr = function(response, status, message) {
@@ -51,7 +31,86 @@ var rspnErr = function(response, status, message) {
 	response.end(message);
 };
 
-var request = require('request');
+var serveFile = function(jsonUrl, succ, fail) {
+	var pathname = jsonUrl.pathname;
+	var extn = path.parse(pathname).ext;
+
+	if (extn) {
+		fs.readFile(path.join('.', pathname), function(error, data) {
+				if (error) {
+					if (error.code === 'ENOENT') {
+						fail(404, 'File ' + pathname + ' not found');
+					} else {
+						throw err;
+					}
+				} else {
+					succ('Content-type', mimeMap[extn] || 'text/plain', data);
+				}
+		});
+	}
+};
+
+var buildUi = function(msg, user, yr, mn, dt, step, node, state) {
+	var ro0 = (state != 1) ? 'readonly' : '';
+	var ro1 = ((state == 2) || (state == 3)) ? 'readonly' : '';
+	var fcs = (state == 0) ? "'step'" : "'user'";
+	var act = (state == 0) ? '/submit' : ((state == 1) ? '/verify' : '/');
+
+	var hfs = '<a href="/submit?node=' + node + '">';
+	var hfv = '<a href="/verify?node=' + node + '">';
+	var mnu = (state == 0) ? 'Submit | ' + hfv + 'Verify</a>' : ((state == 1) ? hfs + 'Submit</a> | Verify' : ((state < 0) ? hfs + 'Submit</a> | ' + hfv + 'Verify</a>' : 'Submit | Verify'));
+	var rtn = (state == 2) ? hfs + 'Return</a></div>' : ((state == 3) ? hfv + 'Return</a></div>' : '<input type="submit" value="Send"/><input type="reset" value="Clear"/>');
+
+	var slt = '';
+	if ((state >= 0) && (state != 2) && (state != 3)) {
+		slt = ' | Connect to <select name="node" value="' + node + '" style="font-size:0.7em;" onchange="this.form.submit()">'
+		for (var i = 0; i < bcNodes.length; i ++) {
+			slt += '<option ' + ((i == node) ? 'selected ' : '') + 'value="' + i + '">' + bcNodes[i].name + '</option>';
+		}
+		slt += '</select>';
+	}
+
+	var hdr =
+'<html><head><title>HKICL Pedometer Contest</title></head>' +
+'<body style="font-family:sans-serif" onload="document.getElementById(' + fcs + ').focus();">' +
+'<h2><img src="logo.png" style="vertical-align:middle;width:72;height:72"/>HKICL Pedometer Contest</h2>' +
+'<form action="' + act + '" method="get"><div style="font-size:0.9em;height:21px"><a href="/">Home</a> | ' + mnu + slt + '</div></form>' +
+'<div style="font-size:1.1em;height:28px">' + ((msg == '') ? 'Welcome' : msg) + '</div>';
+
+	var ctn = (state < 0) ?
+('<h1>Draw some blockchains here!</1>')
+:
+('<form action="' + act + '" method="post"><input type="hidden" name="node" value="' + node + '"/><table>' +
+ '<tr><td>User: </td><td><input type="text" name="user" id="user" value="' + user + '" ' + ro0 + '/></td></tr>' +
+ '<tr><td>Date: </td><td>' +
+ '<input type="text" size="4" maxlength="4" name="year" value="' + yr + '" ' + ro1 + '/>' +
+ '<input type="text" size="2" maxlength="2" name="mnth" value="' + mn + '" ' + ro1 + '/>' +
+ '<input type="text" size="2" maxlength="2" name="date" value="' + dt + '" ' + ro1 + '/></td></tr>' +
+ '<tr><td>Steps: </td><td>' + '<input type="text" size="7" name="step" id="step" value="' + step + '" onfocus="this.select();" ' + ro1 + '/></td></tr>' +
+ '</table><br/>' + rtn + '</form>');
+
+	var ftr = '</body></html>';
+
+	return (hdr + ctn + ftr);
+};
+
+// Webservices
+var queryChain = function(node, succ, fail, blck) {
+	request(protocol + '://' + bcNodes[node].addr + ':' + bcNodes[node].port + '/chain' + ((blck) ? '/block/' + blck : ''),
+		function (error, response, body) {
+			if (error) {
+				fail('Read chain failed', error);
+			} else {
+				switch (response.statusCode) {
+				case 200:
+					succ(body);
+					break;
+				default:
+					fail('Read chain result in status ' + response.statusCode, response);
+				}
+			}
+	});
+};
 
 var enroll = function(node, succ, fail) {
 	console.log('Enrolling ' + bcNodes[node].user + ' (' + bcNodes[node].scrt + ')');
@@ -69,7 +128,6 @@ var enroll = function(node, succ, fail) {
 					//enroll successful
 					succ(body);
 					break;
-
 				default:
 					fail('Enrollment result in status ' + response.statusCode, response);
 				}
@@ -113,7 +171,6 @@ var deploy = function(node, succ, fail) {
 	});
 };
 
-var http = require('http');
 var node = -1;
 var ccid = {};
 
@@ -126,11 +183,8 @@ http.createServer(function(req, res) {
 			console.error(err);
 	});
 
-	var url = require('url');
 	var rqst = url.parse(req.url, true);
 
-	const os = require('os');
-	var path = require('path');
 	var userName = null;
 	switch (os.platform()) {
 	case 'win32':
@@ -174,7 +228,7 @@ http.createServer(function(req, res) {
 								if (body.result.status == 'OK') {
 									ccid[node] = body.result.message;
 								}
-								res.end(buildUi("Deploy successful", userName, now.getFullYear(), (now.getMonth() + 1), (now.getDate() - 1), 0, node, 0));
+								res.end(buildUi("Deploy successful", userName, now.getFullYear(), (now.getMonth() + 1), (now.getDate() - 1), 0, node, -1));
 							},
 							function(mssg, errr) {
 								rspnErr(res, 500, mssg);
@@ -192,10 +246,13 @@ http.createServer(function(req, res) {
 				case '/':
 					// Main page listing data
 					if (ccid) {
-						res.end('CCID is ' + ccid);
-					} else {
-						res.end('CCID is unspecified');
-						
+						queryChain(node,
+							function(body) {
+								res.end(buildUi(body, userName, now.getFullYear(), (now.getMonth() + 1), (now.getDate() - 1), 0, node, -1));
+							},
+							function(mssg, errr) {
+								rspnErr(res, 500, mssg);
+						});
 					}
 					break;
 
@@ -208,7 +265,14 @@ http.createServer(function(req, res) {
 					break;
 
 				default:
-					rspnErr(res, 404, rqst.pathname + ' not found');
+					serveFile(rqst,
+						function(key, hdr, ctn) {
+							res.setHeader(key, hdr);
+							res.end(ctn);
+						},
+						function(sts, ctn) {
+							rspnErr(res, sts, ctn);
+					});
 				}
 				break;
 
