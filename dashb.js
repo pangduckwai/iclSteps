c3f4d33d737c6ae34c8de64eae2dd0d12d2a826c
pangduckwai <paul.lai@sea9.org>
2017-04-24 10:56:54 -0700

    Fix block drawing logic

--- dashb.js
+++ dashb.js
@@ -4,16 +4,18 @@ if ((/*@cc_on ! @*/ false) || navigator.userAgent.match(/Trident/g)) {
 	isIE = true;
 }
 
-const RUN_INTERVAL = 1000;
+const RUN_INTERVAL = 3000;
 const MAX_BLOCK_DSP = 10;
-const MARGIN_LEFT = 10;
+const MARGIN_LEFT = 15;
+const BLOCK_SIDE_LEN = 40;
 
 var threadId = null;
 
 function init() {
-	threadId = setInterval(function() {
+	/*threadId = setInterval(function() {
 			render();
-	}, RUN_INTERVAL);
+	}, RUN_INTERVAL);*/
+	render();
 }
 
 function stop() {
@@ -22,8 +24,11 @@ function stop() {
 }
 
 function render() {
+	var uldim = d3.select(".charts").node().getBoundingClientRect();
+
 	// Draw block-list
 	var blockList = d3.select("#block-list");
+	blockList.style("top", uldim.y + 5).style("left", 5);
 	var dimen = blockList.node().getBoundingClientRect();
 
 	var grph = blockList.select(".chart-ctnt .chart-viz");
@@ -36,50 +41,56 @@ function render() {
 	}
 
 	var blockArray = [];
-	var blockCount = 7; //TODO: get from the chain...
+	var blockCount = 10; //TODO: get from the chain...
 	for (var idx = 0; idx < blockCount; idx ++) {
 		blockArray[idx] = idx;
 	}
-	if (blockArray.length > MAX_BLOCK_DSP) blockArray.length = MAX_BLOCK_DSP; //truncate
-	var blocksWidth = (dimen.width / (MAX_BLOCK_DSP-1)) * (blockArray.length-1);
-	var scaleX = d3.scale.ordinal().domain(blockArray.map(function(d) {return d;})).range([0, blocksWidth]);
+	if (blockArray.length > MAX_BLOCK_DSP) blockArray.length = MAX_BLOCK_DSP; //TODO: truncate
+	var blocksWidth = ((dimen.width - MARGIN_LEFT) / MAX_BLOCK_DSP) * blockArray.length;
+	var scaleX = d3.scale.ordinal().domain(blockArray).rangeRoundPoints([0, blocksWidth]);
 
 	var tickDistance = MARGIN_LEFT; // Any value
 	if (blockArray.length > 1) {
-		tickDistance = scaleX(blockArray[1]) - scaleX(blockArray[0]);
+		tickDistance = scaleX(blockArray[blockArray.length - 1]) - scaleX(blockArray[blockArray.length - 2]);
 	}
+	console.log(new Date(), dimen.width, blocksWidth, tickDistance); //TODO TEMP
+
+	var BLOCK_SIDE_HLF = BLOCK_SIDE_LEN / 2;
+	var BLOCK_SIDE_X = 0.6 * BLOCK_SIDE_LEN;
+	var BLOCK_SIDE_Y = -0.4 * BLOCK_SIDE_LEN;
+	var BLOCK_LINE_Y = 0.3 * BLOCK_SIDE_LEN;
 
 	var blocks = grph.selectAll("block").data(blockArray, function(d) { return d; } );
 	var block = blocks.enter().append("g").attr("class", "block").attr("transform", "translate(0, " + (dimen.height / 2) + ")");
 	block.append("rect").attr("class", "block-rect")
 		.attr("x", function(d) { return scaleX(d) + MARGIN_LEFT; })
-		.attr("y", 0).attr("width", 20).attr("height", 20);
+		.attr("y", 0).attr("width", BLOCK_SIDE_LEN).attr("height", BLOCK_SIDE_LEN);
 	block.append("polygon").attr("class", "block-rect")
 		.attr("points", function(d) {
 				var p0 = [scaleX(d) + MARGIN_LEFT, 0];
-				var p1 = [p0[0] + 12, -8];
-				var p2 = [p1[0] + 20, -8];
-				var p3 = [p0[0] + 20,  0];
+				var p1 = [p0[0] + BLOCK_SIDE_X, BLOCK_SIDE_Y];
+				var p2 = [p1[0] + BLOCK_SIDE_LEN, BLOCK_SIDE_Y];
+				var p3 = [p0[0] + BLOCK_SIDE_LEN,  0];
 				return [p0.join(","), p1.join(","), p2.join(","), p3.join(",")].join(" ");
 		});
 	block.append("polygon").attr("class", "block-rect")
 		.attr("points", function(d) {
-				var p0 = [scaleX(d) + MARGIN_LEFT + 20,  0];
-				var p1 = [p0[0] + 12, -8];
-				var p2 = [p1[0], 12];
-				var p3 = [p0[0], 20];
+				var p0 = [scaleX(d) + MARGIN_LEFT + BLOCK_SIDE_LEN,  0];
+				var p1 = [p0[0] + BLOCK_SIDE_X, BLOCK_SIDE_Y];
+				var p2 = [p1[0], BLOCK_SIDE_X];
+				var p3 = [p0[0], BLOCK_SIDE_LEN];
 				return [p0.join(","), p1.join(","), p2.join(","), p3.join(",")].join(" ");
 		});
 	block.append("line").attr("class", "block-line")
-		.attr("x1", function(d) { return scaleX(d) + MARGIN_LEFT - tickDistance + 26; }).attr("y1", 6)
-		.attr("x2", function(d) { return scaleX(d) + MARGIN_LEFT; }).attr("y2", 6);
+		.attr("x1", function(d) { return scaleX(d) + MARGIN_LEFT - tickDistance + BLOCK_SIDE_LEN + BLOCK_LINE_Y; }).attr("y1", BLOCK_LINE_Y)
+		.attr("x2", function(d) { return scaleX(d) + MARGIN_LEFT; }).attr("y2", BLOCK_LINE_Y);
 	block.append("line").attr("class", "block-line")
-		.attr("x1", function(d) { return scaleX(d) + MARGIN_LEFT + 26; }).attr("y1", 6)
-		.attr("x2", function(d) { return scaleX(d) + MARGIN_LEFT + tickDistance; }).attr("y2", 6);
+		.attr("x1", function(d) { return scaleX(d) + MARGIN_LEFT + BLOCK_SIDE_LEN + BLOCK_LINE_Y; }).attr("y1", BLOCK_LINE_Y)
+		.attr("x2", function(d) { return scaleX(d) + MARGIN_LEFT + tickDistance; }).attr("y2", BLOCK_LINE_Y);
 	block.append("text").attr("class", "block-text")
 		.text(function(d) { return d; })
-		.attr("x", function(d) { return scaleX(d) + MARGIN_LEFT + 10; })
-		.attr("y", 10).attr("text-anchor", "middle").attr("dominant-baseline", "middle");
+		.attr("x", function(d) { return scaleX(d) + MARGIN_LEFT + BLOCK_SIDE_HLF; })
+		.attr("y", BLOCK_SIDE_HLF).attr("text-anchor", "middle").attr("dominant-baseline", "middle");
 
 	blocks.exit().remove();
 }
