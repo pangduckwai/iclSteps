b7f8c95259fe5ad61f9f0055a48a7aadb3ae3722
icdlct <paul.ct.lai@hkicl.com.hk>
2017-06-01 14:05:40 +0800

    Update chaincode - test 1

--- chaincode/steps.go
+++ chaincode/steps.go
@@ -7,6 +7,19 @@ import (
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
+const keyVersion string = "version"
+const keyRecord string = "record"
+
+type StruResult struct {
+	Status string `json:"status"`
+	Message string `json:"message"`
+}
+type StruResponse struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Result StruResult `json:"result"`
+	Id int `json:"id"`
+}
+
 type SimpleChaincode struct {
 }
 
@@ -23,7 +36,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	}
 
 	now := time.Now().Local()
-	err := stub.PutState("version", []byte(now.Format("20060102150405")))
+	err := stub.PutState(keyVersion, []byte(now.Format("20060102150405")))
 	if err != nil {
 		return nil, err
 	}
@@ -32,8 +45,6 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 }
 
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println(" >>>>>>> Invoking " + function)
-
 	if function == "init" {
 		return t.Init(stub, "init", args)
 	} else if function == "write" {
@@ -46,8 +57,6 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 }
 
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println(" >>>>>>> Querying " + function)
-
 	if function == "read" { //read a variable
 		return t.read(stub, args)
 	} else {
@@ -80,11 +89,15 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	var key, jsonResp string
 	var err error
 
-	if len(args) != 1 {
+	if len(args) > 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	key = args[0]
+	key = keyVersion
+	if len(args) == 1 {
+		key = args[0]
+	}
+
 	valAsbytes, err := stub.GetState(key)
 	if err != nil {
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
